98664d9557fbe3382a1bbff65904dc07c929775b
pouria-drd <pouriadrd@gmail.com>
2024-09-02 13:02:18 +0330

    added discount model

--- shop/models/product_model.py
+++ shop/models/product_model.py
@@ -30,10 +30,35 @@ class Product(models.Model):
         return self.name
 
 
+class Discount(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(_("name"), max_length=100)
+    description = models.TextField(_("description"), blank=True, null=True)
+
+    discount_percentage = models.DecimalField(
+        _("discount percentage"), max_digits=5, decimal_places=2
+    )
+
+    is_active = models.BooleanField(_("is active"), default=True)
+    start_date = models.DateTimeField(_("start date"), null=True, blank=True)
+    end_date = models.DateTimeField(_("end date"), null=True, blank=True)
+
+    def __str__(self):
+        return self.name + " - " + self.discount_percentage
+
+
 class Price(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="prices"
+    )
+    discount = models.ForeignKey(
+        Discount,
+        on_delete=models.SET_NULL,
+        related_name="prices",
+        null=True,
+        blank=True,
     )
 
     amount = models.DecimalField(_("amount"), max_digits=10, decimal_places=2)
@@ -52,3 +77,11 @@ class Price(models.Model):
 
     def __str__(self):
         return f"{self.product.name} - {self.amount}"
+
+    def get_final_price(self):
+        """Returns the final price after applying the linked discount."""
+        if self.discount and self.discount.is_active:
+            discount_amount = self.amount * (self.discount.discount_percentage / 100)
+            return self.amount - discount_amount
+
+        return self.amount
